4e56bef879108c98ec6adae109cb4b3bd5ac2fa6
Matthew Tran <mattgtran2001@gmail.com>
2025-09-26 15:15:56 -0700

    Added Dash And Diagonal Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player Settings")]
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float dashDistance = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,21 +17,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = Vector3.zero;
+
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            movement += Vector3.forward;
         }
         if(Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            movement += Vector3.back;
         }
         if(Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            movement += Vector3.left;
         }
         if(Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            movement += Vector3.right;
         }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            movement *= dashDistance;
+        }
+        else
+        {
+            movement *= moveSpeed * Time.deltaTime;
+        }
+
+        transform.Translate(movement);
     }
 }
