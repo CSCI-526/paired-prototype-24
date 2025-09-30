cf31b24e92b48819000d0e7e7d9dffa1df1a025e
Matthew Tran <mattgtran2001@gmail.com>
2025-09-30 16:53:57 -0700

    Enemy Move Towards Players and Die

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Enemy Settings")]
+    [SerializeField] private float moveSpeed = 3f;
+    private Transform player;
+
+    void Start()
+    {
+        PlayerController playerController = FindObjectOfType<PlayerController>();
+        if (playerController != null)
+        {
+            player = playerController.transform;
+        }
+        else
+        {
+            Debug.LogWarning("No PlayerController found in the scene!");
+        }
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Game Over!");
+        }
+        
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
